a2dbb25583ab4750242cb80ba4e7e064b67921e2
Chenming Huang <quic_chenhuan@quicinc.com>
2022-04-10 09:02:08 +0800

    Android: Compile hs20-osu-client to /vendor/bin in test builds
    hs20-osu-client compilation fails on Android O onwards because of
    undefined reference for __android_log_print/__android_log_vprint.
    
    Modify hs20-osu-client's Android.mk to include liblog library and
    use tag 'hs20-osu-client' in logcat logs. Additionally, compile
    hs20-osu-client to /vendor/bin in non-production builds.
    
    Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -7,4 +7,9 @@ ifneq ($(filter VER_0_8_X VER_2_1_DEVEL,$(WPA_SUPPLICANT_VERSION)),)
 # set up in the other Android.mk.
 include $(S_LOCAL_PATH)/hostapd/Android.mk \
         $(S_LOCAL_PATH)/wpa_supplicant/Android.mk
+ifneq ($(TARGET_BUILD_VARIANT), user)
+ifeq ($(shell test $(PLATFORM_VERSION_LAST_STABLE) -ge 8 ; echo $$?), 0)
+include $(S_LOCAL_PATH)/hs20/client/Android.mk
+endif #End of Check for platform version
+endif #End of Check for target build variant
 endif
