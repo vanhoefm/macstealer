698c05da2bd3233b005d45873caa852bc29b32c5
Jouni Malinen <j@w1.fi>
2022-04-16 13:22:16 +0300

    tests: Update server and user certificates (2022)
    The previous versions are going to be expiring soon, so need to re-sign
    these to avoid EAP test case failures. This contains updates from
    running tests/hwsim/auth_server/update.sh.
    
    Signed-off-by: Jouni Malinen <j@w1.fi>

--- tests/hwsim/test_ap_eap.py
+++ tests/hwsim/test_ap_eap.py
@@ -2761,7 +2761,7 @@ def test_ap_wpa2_eap_ttls_server_cert_hash(dev, apdev):
     """WPA2-Enterprise connection using EAP-TTLS and server certificate hash"""
     check_cert_probe_support(dev[0])
     skip_with_fips(dev[0])
-    srv_cert_hash = "5891bd91eaf977684e70d4376d1514621d18f09ab2020bea1ad293d59a6e8944"
+    srv_cert_hash = "37bbb34133f7a3aa56056aae457a0ec7f55ed59fe8fd71107f8ad4a9c81e339f"
     params = hostapd.wpa2_eap_params(ssid="test-wpa2-eap")
     hapd = hostapd.add_ap(apdev[0], params)
     dev[0].connect("test-wpa2-eap", key_mgmt="WPA-EAP", eap="TTLS",
@@ -4206,7 +4206,7 @@ def ocsp_req(outfile):
            "-reqout", outfile,
            '-issuer', 'auth_serv/ca.pem',
            '-sha256',
-           '-serial', '0xD8D3E3A6CBE3CD69',
+           '-serial', '0xD8D3E3A6CBE3CD73',
            '-no_nonce']
     run_openssl(arg)
     if not os.path.exists(outfile):
