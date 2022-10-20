70d89f90e621a7c82c8146f231665d0352f581b7
Sunil Dutt <quic_usdutt@quicinc.com>
2022-10-20 12:05:13 +0530

    A vendor roam control configuration for delaying hand off for RX
    Introduce a run time roam configuration for "hand off delay for RX".
    This value, in milliseconds, will delay the hand off for the specified
    time to receive pending RX frames from the current BSS.
    
    Signed-off-by: Jouni Malinen <quic_jouni@quicinc.com>

--- src/common/qca-vendor.h
+++ src/common/qca-vendor.h
@@ -5101,6 +5101,11 @@ enum qca_vendor_attr_roam_candidate_selection_criteria {
  *	If the current RX link speed is above the threshold, roaming is not
  *	needed. If this attribute is not configured, or if it is set to 0, the
  *	driver will not consider the RX link speed in the roaming decision.
+ *
+ * @QCA_ATTR_ROAM_CONTROL_HO_DELAY_FOR_RX: u16 value in milliseconds.
+ *	Optional parameter. This configuration delays hand-off by the
+ *	specified duration to receive pending RX frames from the current BSS.
+
  */
 enum qca_vendor_attr_roam_control {
 	QCA_ATTR_ROAM_CONTROL_ENABLE = 1,
@@ -5127,6 +5132,7 @@ enum qca_vendor_attr_roam_control {
 	QCA_ATTR_ROAM_CONTROL_SCAN_6G_PSC_DWELL_TIME = 22,
 	QCA_ATTR_ROAM_CONTROL_SCAN_6G_NON_PSC_DWELL_TIME = 23,
 	QCA_ATTR_ROAM_CONTROL_LINKSPEED_THRESHOLD = 24,
+	QCA_ATTR_ROAM_CONTROL_HO_DELAY_FOR_RX = 25,
 
 	/* keep last */
 	QCA_ATTR_ROAM_CONTROL_AFTER_LAST,
